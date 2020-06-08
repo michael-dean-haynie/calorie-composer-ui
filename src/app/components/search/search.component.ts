07563bd257b2433e73443a6e93d06774b5aac99d
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-06-08 17:15:43 -0400

    Wire basic search bar up to api - logging results in console

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FdcApiService } from 'src/app/services/api/fdc-api.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fdcApiService: FdcApiService) { }
 
   ngOnInit(): void {
+  }
+
+  search(event: Event, query: string): void {
+    // Do not actually submit form (don't want page to re-load)
+    event.preventDefault();
+
+    // make request with query
+    this.fdcApiService.search(query).subscribe(result => {
+      console.log(result);
+    });
   }
 
 }
